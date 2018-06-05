ee044fbcf1f7083352c8ecf07ec856aa3d0d6710
hadeslee <7189444+hadesleejs@users.noreply.github.com>
2018-06-05 10:37:10 +0800

    ogo

--- create_lvm_backend.sh
+++ create_lvm_backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dd if=/dev/zero of=/var/lib/cinder/vg_cinder.img bs=1M count=1024
-losetup /dev/loop0 /var/lib/cinder/vg_cinder.img 
+dd if=/dev/zero of=/home/vg_cinder.img bs=1M count=102400
+losetup /dev/loop0 /home/vg_cinder.img 
 partprobe /dev/loop0
 pvcreate /dev/loop0
 vgcreate vg_cinder /dev/loop0
@@ -24,6 +24,7 @@ sleep 1
 service openstack-cinder-volume restart
 service openstack-nova-compute restart
 enabled_backends = lvm
+enabled_backends
 [lvm]
 volume_driver = cinder.volume.drivers.lvm.LVMVolumeDriver
 volume_group = vg_cinder
@@ -37,3 +38,30 @@ service openstack-nova-compute restart
 
 cinder-manage service remove cinder-volume con2@lvm
 cinder-manage service remove BINARY_NAME HOST_NAME
+
+openstack compute service list
+cinder reset-state 86645f-1b48-480d-9a32-ccf4c0dcd1 --state available
+neutron net-delete 6a89b2b5-0c34-49f1-b3fe-e81c653a283f
+service neutron-dhcp-agent restart
+：.,$d
+neutron agent-list  
+rabbitmqctl cluster_status
+/sbin/service rabbitmq-server start
+
+glance image-create --name CentOS_64_template_newest --container-format bare --disk-format qcow2 --file /home/CentOS_64_template_newest-disk1.qcow2 --progress
+
+select id, status, display_name from volumes where id='499c58d9-82ae-4ca4-91b5-7a6954896bdf';
+
+
+update volumes set deleted=1 where id='499c58d9-82ae-4ca4-91b5-7a6954896bdf';
+yum install python-pip
+pip install shadowsocks
+ssserver -p 8388 -k password -m aes-256-cfb  --log-file /tmp/ss.log -d start
+iptables -A INPUT -p tcp --dport 8388 -j ACCEPT
+
+
+ovs-vsctl del-port br-enp9s0f0 enp9s0f0
+		  add-port
+
+
+		  
